35a413dfd8ae45d099de22a71ee1321b4e423381
lintang-b-s <lintangbirdasaputra@gmail.com>
2023-05-09 12:23:50 +0700

    add dockercompose file

--- discovery-server/src/main/java/com/lintang/netflik/discoveryserver/config/SecurityConfig.java
+++ discovery-server/src/main/java/com/lintang/netflik/discoveryserver/config/SecurityConfig.java
@@ -2,37 +2,45 @@ package com.lintang.netflik.discoveryserver.config;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig  {
 
-    @Value("${eureka.username}")
-    private String username;
-    @Value("${eureka.password}")
-    private String password;
-
-    @Override
-    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws  Exception {
-        authenticationManagerBuilder.inMemoryAuthentication()
-                .passwordEncoder(NoOpPasswordEncoder.getInstance())
-                .withUser("eureka").password("password")
-                .authorities("USER");
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().ignoringRequestMatchers();
+        return httpSecurity.build();
     }
 
-
-    @Override
-    public void configure(HttpSecurity httpSecurity) throws  Exception {
-        httpSecurity.csrf().disable()
-                .authorizeRequests().anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic();
-    }
+//    @Value("${eureka.username}")
+//    private String username;
+//    @Value("${eureka.password}")
+//    private String password;
+//
+//    @Override
+//    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws  Exception {
+//        authenticationManagerBuilder.inMemoryAuthentication()
+//                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+//                .withUser("eureka").password("password")
+//                .authorities("USER");
+//    }
+//
+//
+//    @Override
+//    public void configure(HttpSecurity httpSecurity) throws  Exception {
+//        httpSecurity.csrf().disable()
+//                .authorizeRequests().anyRequest()
+//                .authenticated()
+//                .and()
+//                .httpBasic();
+//    }
 }
