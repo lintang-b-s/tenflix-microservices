9481d2a613904163cd2b6acb648156ebb04a09cc
Lintang B S <109817421+lintang-b-s@users.noreply.github.com>
2023-08-26 18:27:40 +0700

    fix error when building image

--- order-aggregator-service-go/internal/entity/keycloak.go
+++
@@ -1,9 +0,0 @@
-package entity
-
-type KeycloakUserDto struct {
-	Id        string `json:"id"`
-	Username  string `json:"username"`
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-	Email     string `json:"email"`
-}
